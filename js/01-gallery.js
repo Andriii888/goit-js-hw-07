d374d007b93d7ef15f0d307f6af47748514de9a5
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2022-11-06 23:02:03 +0300

    add1t

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galaryBoxRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function imgValueFromGalleryItems(values) {
+    let arrayValues = values.map(({ preview, original, description }) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${ preview }"
+      data-source="${original}"
+      alt="Image description"
+    />
+  </a>
+</div>
+       `
+    }).join('');
+    return galaryBoxRef.innerHTML += arrayValues;
+}; 
+imgValueFromGalleryItems(galleryItems);
+
+
+galaryBoxRef.addEventListener('click', onZoomClickedImg); 
+
+function onZoomClickedImg(e) { 
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    const imgSrc = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src='${imgSrc}' width="800" height="600">
+`)
+    instance.show();
+
+
+    const escBox = document.querySelector('.basicLightbox');
+    document.addEventListener('keydown', (e) => {
+        if (e.key === "Escape") {
+            escBox.remove();
+       }
+    });
+};
+
